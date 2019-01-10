62ed4d666db209d21ecb4e08beffcbf8c9aea791
cjy <cjy632258@hotmail.com>
2019-01-10 11:01:24 +0800

    utils

--- utils.js
+++ utils.js
@@ -1,5 +1,5 @@
 module.exports = {
-    ab2hex(buffer) {
+    ab2hex (buffer) {
         const hexArr = Array.prototype.map.call(
             new Uint8Array(buffer),
             function (bit) {
@@ -7,5 +7,22 @@ module.exports = {
             }
         )
         return hexArr.join('')
+    },
+    ab2String (buffer) {
+        var result = ''
+        let v = new DataView(buffer)
+        for (var i = 0; i < buffer.byteLength; i++) {
+            result += String.fromCharCode(v.getUint8(i))
+        }
+        return result
+    },
+    string2ab (str) {
+        var ab = new ArrayBuffer(str.length)
+        let v = new DataView(ab)
+
+        for (var i = 0; i < str.length; i++) {
+            v.setUint8(i, str.charCodeAt(i))
+        }
+        return ab
     }
 }
